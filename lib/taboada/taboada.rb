a0229473c1eac0636c32fae80907b0d79396ac20
Ney <rbranta@homail.co.uk>
2015-09-15 23:14:20 -0300

    Implemented loop "while" to calculate 10 times tables multiplication facts

--- lib/taboada/taboada.rb
+++ lib/taboada/taboada.rb
@@ -8,3 +8,17 @@ class Taboada
     p "#{num.to_i} * #{value} = #{value * num.to_i}"
   end
 end
+
+
+p "informe o valor a ser multiplicado por 10:"
+num = gets.chomp
+
+if num =~ /^[0-9]+$/
+  cont = 1
+  res = 0
+  while cont <= 10
+    res = cont * num.to_i
+    puts "#{num} * #{cont} = #{res}"
+    cont = cont + 1
+  end
+end
