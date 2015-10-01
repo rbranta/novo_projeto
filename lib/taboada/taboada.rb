26fd47c2661161c8bf91b2f206ee19760d3ebe4c
Ney <rbranta@homail.co.uk>
2015-10-01 17:45:01 -0300

    Added the loop "range" with "for" to calculate the times tables

--- lib/taboada/taboada.rb
+++ lib/taboada/taboada.rb
@@ -29,3 +29,18 @@ numeroM = gets.chomp
 
 numeroM.to_i.upto(numeroM.to_i) { |multiplicador1| 1.upto(10) {|multiplicador2|
 p "#{multiplicador1} * #{multiplicador2} = #{multiplicador2 * numeroM.to_i}" } }
+
+
+
+p "informe o numero a ser multiplicado na tabela de 10"
+num = gets.chomp
+
+multiplicador = case num.to_i
+when 1..100 then
+  for value in 1..10
+  p "#{num.to_i} * #{value} = #{value * num.to_i}"
+end
+else "O valor indicado para calculo deve ser deve ser entre 1 e 100"
+end
+
+p multiplicador
